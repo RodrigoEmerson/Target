9416035378a7523d1d022b95aaefbc64e51d6e5e
RodrigoEmerson <127218838+RodrigoEmerson@users.noreply.github.com>
2023-03-10 11:06:30 -0300

    Update Target_questão_04.py

--- Target_questão_04.py
+++ Target_questão_04.py
@@ -12,8 +12,8 @@ perc_mg = (mg / total) * 100
 perc_es = (es / total) * 100
 perc_outros = (outros / total) * 100
 
-print("SP: %.2f%%" % perc_sp)
-print("RJ: %.2f%%" % perc_rj)
-print("MG: %.2f%%" % perc_mg)
-print("ES: %.2f%%" % perc_es)
-print("Outros: %.2f%%" % perc_outros)
+print('SP: {:.2f} %'.format(perc_sp))
+print('RJ: {:.2f} %'.format(perc_rj))
+print('MG: {:.2f} %'.format(perc_mg))
+print('ES: {:.2f} %'.format(perc_es))
+print('Outros: {:.2f} %'.format(perc_outros))
